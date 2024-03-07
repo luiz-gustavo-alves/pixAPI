b77b6b4abe8c678708a6d4d27bee1ceb526444a8
Luiz Gustavo <luizgustavoalves702@gmail.com>
2024-03-06 21:13:36 -0300

    config: postgres database connection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
 using pixAPI.Services;
+using pixAPI.Middlewares;
+using pixAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +13,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<HealthService>();
+
+// Database
+builder.Services.AddDbContext<AppDBContext>(opts =>
+{
+    string host = builder.Configuration["Database:Host"] ?? string.Empty;
+    string port = builder.Configuration["Database:Port"] ?? string.Empty;
+    string username = builder.Configuration["Database:Username"] ?? string.Empty;
+    string database = builder.Configuration["Database:Name"] ?? string.Empty;
+    string password = builder.Configuration["Database:Password"] ?? string.Empty;
+
+    string connectionString = $"Host={host};Port={port};Username={username};Password={password};Database={database}";
+    opts.UseNpgsql(connectionString);
+});
 
 var app = builder.Build();
 
@@ -24,5 +41,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Middlewares
+app.UseMiddleware<ExceptionHandlerMiddleware>();
 
 app.Run();
