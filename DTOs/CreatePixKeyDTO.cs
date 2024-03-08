949a78a1b981bb45b1d49c1736ce63b55b51b16b
Luiz Gustavo <luizgustavoalves702@gmail.com>
2024-03-08 16:27:55 -0300

    fix(DTO): regex for number/agency account for pixKeyCreation

--- DTOs/CreatePixKeyDTO.cs
+++ DTOs/CreatePixKeyDTO.cs
@@ -30,11 +30,13 @@ public class UserCreatePixKeySchema
 public class AccountCreatePixKeySchema
 {
   [Required(ErrorMessage = "{0} é obrigatório.")]
+  [RegularExpression(@"^\d+$", ErrorMessage = "{0} precisa ser um número de conta válido")]
   [StringLength(8, MinimumLength = 4, ErrorMessage = "{0} precisa estar entre 4 e 8 caracteres.")]
   [DataMember(Name = "number")]
   public string Number { get; set; } = null!;
 
   [Required(ErrorMessage = "{0} é obrigatório.")]
+  [RegularExpression(@"^\d+$", ErrorMessage = "{0} precisa ser um número de agência válido")]
   [StringLength(4, MinimumLength = 3, ErrorMessage = "{0} precisa estar entre 3 e 4 caracteres.")]
   [DataMember(Name = "agency")]
   public string Agency { get; set; } = null!;
